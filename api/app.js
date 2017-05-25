d6cbf81880d3e3421929cd73e92534f97c16b14e
Chris Tran <christopher.v.tran@gmail.com>
2017-05-25 02:14:31 -0500

    abstracted out app config from server file. implemented https protocol for server.

--- api/app.js
+++ api/app.js
@@ -1,11 +1,28 @@
 'use strict';
-
-var express = require('express');
-var app = express();
-var config = require('./config/config').config;
+var app = module.exports = require('express')();
 var path = require('path');
+var config = require('./config/config').config;
+var routes = require('./routes');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+// import authentication config
+var authConfig = require(path.join(__dirname, '/utils/authentication/', config.authentication, 'config'));
 
 // set database connection to app locals property
 app.locals.db = require(path.join(__dirname, '/db/', config.database, '/services/db-connection'));
+app.locals.secret = config.secret;
 
-module.exports = app;
+// initialize logger
+app.use(morgan('dev'));
+
+// initialize body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+
+    extended: true
+
+}));
+
+// root path is /* and includes all routes located in 'routes' folder
+app.use('/', routes);
--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
 'use strict';
 
+var https = require('https');
+var fs = require('fs');
 var app = require('./app');
 var port = process.env.PORT || 3000;
 
 // initialize database connection
 app.locals.db.connect();
 
-// initialize server connection
-app.listen(port, () => {
+// initialize secure server connection
+var server = https.createServer({
 
-    console.log('Server is running on port ' + port);
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
+
+}, app).listen(port, () => {
+
+    var serverPort = server.address().port;
+
+    console.log('Server is running at port ' + serverPort);
 
 });
