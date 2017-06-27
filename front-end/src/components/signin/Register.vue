dca2d843597635e2eaaad6e8e3766796f1ef978a
Chris Tran <christopher.v.tran@gmail.com>
2017-06-27 03:26:37 -0500

    added a lot of functionality to the registration page.

--- front-end/src/components/signin/Register.vue
+++ front-end/src/components/signin/Register.vue
@@ -1,30 +1,58 @@
 <template>
 <div class="register">
   <b-card>
-    <form @submit.prevent="handleRegister">
+    <form @submit.prevent="handleSubmit">
       <h2>Register</h2>
+
+      <small class="text-muted">First Name</small>
+      <b-form-input v-model="firstName" id="input-name" type="text" required autofocus />
+      <small class="text-muted">Last Name</small>
+      <b-form-input v-model="lastName" id="input-name" type="text" required />
       <small class="text-muted">Email</small>
-      <b-form-input v-model="email" id="input-email" type="email" required autofocus />
+      <b-form-input v-model="email" id="input-email" type="email" required />
       <small class="text-muted">Password</small>
       <b-form-input v-model="password" id="input-password" type="password" required />
       <small class="text-muted">Confirm password</small>
       <b-form-input v-model="password2" id="input-password2" type="password" required />
-      <b-form-radio v-model="accountType" :options="accountOptions" required>
-      </b-form-radio>
+
+      <b-form-radio v-model="accountType" :options="accountOptions" required />
+
       <b-button id="btn-register">Register</b-button>
     </form>
     <b-button id="already-member" variant="link" to="signin">Already a member?</b-button>
   </b-card>
+
+  <b-modal id="register-client-modal" title="Coach selection">
+    Who is your coach?
+    <b-form-select :options="coachList" v-model="coach" />
+    <footer slot="modal-footer">
+      <b-btn variant="secondary" @click="handleCancel">Cancel</b-btn>
+      <b-btn variant="primary" @click="handleRegisterClient">Select</b-btn>
+    </footer>
+  </b-modal>
+
+  <b-modal id="registration-results-modal" title="Registration confirmation">
+    {{ message }}
+    <footer slot="modal-footer">
+      <b-btn variant="primary" @click="handleClose('registration-results-modal')">Ok</b-btn>
+    </footer>
+  </b-modal>
 </div>
 </template>
 
 <script>
 import api from '@/../utils/api';
+import { signin } from '@/../utils/auth';
+import Router from 'vue-router';
+
+var router = new Router();
 
 export default {
   name: 'register',
   data () {
     return {
+      firstName: '',
+      lastName: '',
       email: '',
       password: '',
       password2: '',
@@ -38,40 +66,146 @@ export default {
           text: 'Client',
           value: 'client'
         }
-      ]
+      ],
+      coachList: [],
+      coach: undefined,
+      message: ''
     };
   },
   methods: {
-    comparePasswords() {
-      return this.password === this.password2 ? true : false;
+    handleClose(modalId) {
+      this.$root.$emit('hide::modal', modalId);
     },
-    handleRegister() {
-      if (!this.comparePasswords()) {
-        alert('Your passwords do not match.');
-      } else {
-        var payload = {
-          email: this.email,
-          password: this.password,
-          accountType: this.accountType,
-          memberStart: new Date().toISOString()
-        };
+    handleOpen(modalId) {
+      this.$root.$emit('show::modal', modalId);
+    },
+    getCoachList() {
+      var query = '?accountType=coach';
 
-        api.post('/users', payload)
-          .then((response) => {
-            if (response.status === 201) {
-              alert('Your account was created successfully!');
+      api.get('/users/' + query)
+        .then((response) => {
+          if (response.status === 200) {
+            for (var i = 0; i < response.data.users.length; i++) {
+              var fullName = response.data.users[i].lastName + ', ' + response.data.users[i].firstName;
+
+              this.coachList.push({
+                text: fullName,
+                value: response.data.users[i].email
+              });
             }
-          })
-          .catch((error) => {
-            alert('Account already exists.');
+
+            this.coachList.sort();
+          }
+        })
+        .catch((error) => {
+          console.log('No coaches found.');
+        });
+    },
+    handleSubmit() {
+      if (this.password !== this.password2) {
+        this.message = 'Your passwords do not match.';
+        this.handleOpen('registration-results-modal');
+      } else if (this.accountType === '') {
+        this.message = 'Account type is required.';
+        this.handleOpen('registration-results-modal');
+      } else if (this.accountType === 'client') {
+        this.handleRegister()
+          .then((result) => {
+            if (result) {
+              signin(this.email, this.password)
+                .then((result) => {
+                  if (result) {
+                    this.getCoachList();
+                    this.handleOpen('register-client-modal');
+                  }
+                });
+            } else {
+              this.handleOpen('registration-results-modal');
+            }
+          });
+      } else {
+        this.handleRegister()
+          .then((result) => {
+            this.handleOpen('registration-results-modal');
           });
       }
+    },
+    handleRegister() {
+      var payload = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+        accountType: this.accountType,
+        memberStart: new Date().toISOString()
+      };
+
+      return api.post('/users', payload)
+        .then((response) => {
+          if (response.status === 201) {
+            this.message = 'Your account was created successfully!';
+            return true;
+          }
+        })
+        .catch((error) => {
+          this.message = 'There was an error creating your account.';
+          return false;
+        });
+    },
+    handleOk() {
+      if (this.message === 'Your account was created successfully!') {
+        signin(this.email, this.password)
+          .then((result) => {
+            if (result) {
+              router.push('/dash');
+            }
+          });
+      } else {
+        this.handleClose('registration-results-modal');
+      }
+    },
+    handleRegisterClient() {
+      var query = this.email;
+      var payload = {
+        coach: this.coach
+      };
+
+      api.patch('/users/' + query, payload)
+        .then((response) => {
+          if (response.status === 200) {
+            router.push('/dash');
+          } else {
+            this.handleClose('register-client-modal');
+          }
+        })
+        .catch((error) => {
+          console.log('There was an error assigning your coach: ', error);
+          this.handleClose('register-client-modal');
+        });
+    },
+    handleCancel() {
+      var query = this.email;
+
+      api.delete('/users/' + query)
+        .then((response) => {
+
+        })
+        .catch((error) => {
+
+        });
+
+      this.coach = undefined;
+      this.coachList = [];
+      this.handleClose('register-client-modal');
     }
   }
 };
 </script>
 
 <style scoped>
+button {
+  cursor: pointer;
+}
 .register {
   max-width: 330px;
   padding: 60px 15px;
@@ -81,7 +215,7 @@ export default {
   padding: 15px 0px;
   border-color: #cccccc;
   background-color: #f6f6f6;
-  height: 420px;
+  height: 575px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
               0 6px 20px 0 rgba(0, 0, 0, 0.19);
   -moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
@@ -110,7 +244,6 @@ h2 {
   background-color: #0A3E65;
   display: block;
   width: 100%;
-  cursor: pointer;
 }
 #btn-register:hover, #btn-register:active {
   background-color: #225174;
