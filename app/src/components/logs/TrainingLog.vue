3ef5a8db494ea60372750ecc8fbcb9008a03979e
Chris Tran <christopher.v.tran@gmail.com>
2018-05-24 17:07:30 +1000

    not sure. just syncing repo.

--- app/src/components/logs/TrainingLog.vue
+++ app/src/components/logs/TrainingLog.vue
@@ -6,16 +6,17 @@
       <div class="program-month" :style="{ gridTemplateColumns: columns }">
         <div class="program-day-column">
           <span class="filler-day-label"></span>
-          <span class="day-label" v-for="n in days">Day {{ n }}</span>
+          <span class="day-label" v-for="n in days" :key="n">Day {{ n }}</span>
         </div>
-        <div class="program-week" v-for="(week, index) in weeks">
+        <div class="program-week" v-for="(week, index) in weeks" :key="index">
           <span class="week-label">Week {{ index + 1 }}</span>
           <div class="program-day"
             v-for="day in week"
+            :key="day"
             @click.stop="handleOpenLog(day)"
           >
             <ul class="exercise-list">
-              <li v-for="exercise in day">
+              <li v-for="(exercise, index) in day" :key="index">
                 <strong>{{ exercise.variations }}{{ exercise.exercise }}</strong>
                 <br />
                 {{ exercise.scheme }} {{ exercise.loadScheme }}
@@ -28,21 +29,57 @@
 
     <v-dialog
       lazy
+      :width="720"
       v-model="dialog"
     >
       <v-card>
         <v-card-title>
-          <span class="headline">Session log - {{ formattedDate }}</span>
+          <span class="headline">Session log -- {{ formattedDate }}</span>
         </v-card-title>
         <v-card-text>
-          <v-text-field label="Weigh in" v-model="log.weighIn"></v-text-field>
-          <div class="session-log" v-for="(exercise, index) in log.exercises">
-            <span>{{ exercise.variations }} {{ exercise.exercise }}</span>
-            <v-text-field label="Sets" v-model="log.exercises[index].sets"></v-text-field>
-            <v-text-field label="Reps" v-model="log.exercises[index].reps"></v-text-field>
-            <v-text-field label="Weight" v-model="log.exercises[index].weight"></v-text-field>
-            <v-text-field label="RPE" v-model="log.exercises[index].rpe"></v-text-field>
-          </div>
+          <v-layout row>
+            <v-flex xs2>
+              <v-text-field
+                class="ma-0 pb-0"
+                label="Weigh in"
+                single-line
+                v-model="log.weighIn"></v-text-field
+              >
+            </v-flex>
+          </v-layout>
+          <v-layout
+            row
+            v-for="(exercise, index) in log.exercises"
+            :key="index"
+          >
+            <v-flex xs4>
+              <v-subheader class="mr-4 pt-4">{{ exercise.variations }}{{ exercise.exercise }}</v-subheader>
+            </v-flex>
+            <v-text-field
+              class="ma-0 mr-3 pb-0"
+              label="Sets"
+              single-line
+              v-model="log.exercises[index].sets"
+            ></v-text-field>
+            <v-text-field
+              class="ma-0 mr-3 pb-0"
+              label="Reps"
+              single-line
+              v-model="log.exercises[index].reps"
+            ></v-text-field>
+            <v-text-field
+              class="ma-0 mr-3 pb-0"
+              label="Weight"
+              single-line
+              v-model="log.exercises[index].weight"
+            ></v-text-field>
+            <v-text-field
+              class="ma-0 mr-3 pb-0"
+              label="RPE"
+              single-line
+              v-model="log.exercises[index].rpe"
+            ></v-text-field>
+          </v-layout>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -57,6 +94,22 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-snackbar
+      :timeout="3000"
+      top
+      multi-line
+      vertical
+      :error="snackbarStatus === 'error'"
+      v-model="snackbar"
+    >
+      {{ snackbarText }}
+      <v-btn
+        flat
+        dark
+        @click.native="snackbar = false"
+      >Close</v-btn>
+    </v-snackbar>
   </div>
 </template>
 
@@ -80,17 +133,27 @@
           'RPE'
         ],
         log: {
+          id: '',
           date: '',
           weighIn: '',
           exercises: []
         },
         formattedDate: '',
-        dialog: false
+        dialog: false,
+        snackbar: false,
+        snackbarStatus: '',
+        snackbarText: ''
       };
     },
     firebase: {
       programs: {
         source: firebasedb.ref('/programs'),
+        cancelCallback(error) {
+          console.error('firebasedb error: ', error);
+        }
+      },
+      trainingLogs: {
+        source: firebasedb.ref('/trainingLogs'),
         cancelCallback(error) {
           console.error('firebasedb error: ', error);
         }
@@ -119,6 +182,25 @@
                 this.formatMicrocycles();
               } else {
                 console.log('No data found.');
+              }
+            });
+      },
+
+      /**
+       * Handles retrieving and populating training log
+       */
+      getTrainingLog: function() {
+        this.$firebaseRefs.trainingLogs
+          .orderByKey()
+          .equalTo(this.id)
+          .once('value')
+            .then((snapshot) => {
+              var data = snapshot.val();
+
+              if (data !== null) {
+                var key = Object.keys(data)[0];
+                var log = JSON.parse(JSON.stringify(data[key]));
+                console.log('getTrainingLog log', log);
               }
             });
       },
@@ -211,143 +293,35 @@
        * @param      {Array}  day     The day
        */
       handleOpenLog: function(day) {
-        this.log.date = new Date().toISOString();
-        this.formattedDate = new Date().toUTCString().slice(0, -13);
+        if (this.log.date === '') {
+          this.log.date = new Date().toISOString();
+          this.formattedDate = new Date().toUTCString().slice(0, -13);
+        }
+  
         this.log.exercises = JSON.parse(JSON.stringify(day));
         this.dialog = true;
+      },
+
+      /**
+       * Handles save training log function
+       */
+      handleSave: function() {
+        var newLogRef = this.$firebaseRefs.trainingLogs.push();
+        this.log.id = newLogRef.toString().match(/-(?:.(?!-))+$/g)[0];
+
+        return newLogRef.set(this.log)
+          .then(() => {
+            this.snackbarStatus = '';
+            this.snackbarText = 'Training log saved successfully.';
+            this.snackbar = true;
+          })
+          .catch((error) => {
+            this.snackbarStatus = 'error';
+            this.snackbarText = 'There was an error saving your training log.';
+            this.snackbar = true;
+            console.log('Error saving training log: ', error);
+          });
       }
-
-      // /**
-      //  * Makes an api call to retrieve log based on session id. If there is no log, sets default log information based on number of exercises in session array.
-      //  *
-      //  * @param      {Object}  session       The session
-      //  * @param      {string}  microcycleId  The microcycle identifier
-      //  */
-      // handleLog(session, microcycleId) {
-      //   var query = '?_session=' + session._id;
-
-      //   api.get('/training/logs' + query)
-      //     .then((response) => {
-      //       this.log.logId = response.data.logs[0]._id;
-      //       this.log.programId = response.data.logs[0].metadata._program;
-      //       this.log.microcycleId = response.data.logs[0].metadata._microcycle;
-      //       this.log.sessionId = response.data.logs[0].metadata._session;
-      //       this.log.date = response.data.logs[0].metadata.date;
-      //       this.log.weighIn = response.data.logs[0].weighIn;
-      //       this.log.exercises = JSON.parse(JSON.stringify(response.data.logs[0].main.concat(response.data.logs[0].accessories)));
-      //     })
-      //     .catch(() => {
-      //       this.log.logId = '';
-      //       this.log.programId = this.$store.state.programs.program._id;
-      //       this.log.microcycleId = microcycleId;
-      //       this.log.sessionId = session._id;
-      //       this.log.weighIn = '';
-      //       this.log.exercises = [];
-
-      //       for (var i = 0; i < session.session.length; i++) {
-      //         var exercise = {
-      //           exercise: session.session[i].name,
-      //           variations: session.session[i].variations.trim()
-      //         };
-
-      //         this.log.exercises.push(exercise);
-      //       }
-      //     });
-
-      //   this.handleOpen('session-log-modal');
-      // },
-
-      // /**
-      //  * Splits the scheme string into key/values
-      //  *
-      //  * @param      {Object}  value   Exercise object
-      //  * @return     {Object}  Formatted exercise scheme
-      //  */
-      // splitSchemeString(value) {
-      //   var regex = /\d*(%|rpe|$)/g;
-      //   var x = value.scheme.indexOf('x');
-      //   var at = value.scheme.indexOf(' @');
-
-      //   var sets = value.scheme.slice(0, x);
-      //   var reps = value.scheme.slice(x + 1, at);
-      //   var load = value.scheme.match(regex)[0].replace('rpe', '');
-
-      //   var loadTypeStart = value.scheme.indexOf(load);
-      //   var loadType = value.scheme.slice(loadTypeStart + load.length).replace('/', '');
-
-      //   return {
-      //     sets: sets,
-      //     reps: reps,
-      //     load: load,
-      //     loadType: loadType
-      //   };
-      // },
-
-      // /**
-      //  * Saves log information
-      //  *
-      //  * @param      {Object}  session  The session
-      //  */
-      // handleSave(session) {
-      //   // prepare payload object
-      //   var payload = {
-      //     metadata: {
-      //       date: new Date().toISOString(),
-      //       _program: this.log.programId,
-      //       _microcycle: this.log.microcycleId,
-      //       _session: this.log.sessionId
-      //     },
-      //     weighIn: this.log.weighIn,
-      //     main: [],
-      //     accessories: []
-      //   };
-
-      //   // prepares and pushes exercise object to either main or accessories array
-      //   for (var i = 0; i < this.log.exercises.length; i++) {
-      //     var exercise = {
-      //       exercise: this.log.exercises[i].exercise,
-      //       variations: this.log.exercises[i].variations,
-      //       sets: this.log.exercises[i].sets,
-      //       reps: this.log.exercises[i].reps,
-      //       weight: this.log.exercises[i].weight,
-      //       rpe: this.log.exercises[i].rpe
-      //     };
-
-      //     if (['squat', 'bench press', 'deadlift'].indexOf(exercise.exercise) === -1) {
-      //       payload.accessories.push(exercise);
-      //     } else {
-      //       payload.main.push(exercise);
-      //     }
-      //   }
-
-      //   // if logId exists (set from api call on handleLog), then makes patch api call, else makes post api call
-      //   if (this.log.logId) {
-      //     var query = this.log.logId;
-
-      //     api.patch('/training/logs/' + query, payload)
-      //       .then((response) => {
-      //         if (response.status === 200) {
-      //           console.log('Log updated successfully.');
-      //         }
-      //       })
-      //       .catch((error) => {
-      //         console.log('Error updating log: ', error);
-      //       });
-      //   } else {
-      //     api.post('/training/logs', payload)
-      //       .then((response) => {
-      //         if (response.status === 201) {
-      //           console.log('Log saved successfully.');
-      //         }
-      //       })
-      //       .catch((error) => {
-      //         console.log('Error saving log: ', error);
-      //       });
-      //   }
-
-      //   this.handleClose('session-log-modal');
-      // }
-
     }
   };
 </script>
@@ -391,6 +365,7 @@
     min-height: 100px;
     background-color: #ffffff;
     padding: 5px;
+    cursor: pointer;
   }
   .program-day:hover {
     background-color: #f9f9f9;
@@ -398,17 +373,5 @@
   .exercise-list {
     margin-bottom: 0;
     padding-left: 20px;
-  }
-  #input-weigh-in {
-    margin-left: 10px;
-    width: 80px;
-    display: inline-block;
-  }
-  .session-log {
-    display: grid;
-    grid-template-columns: 2fr repeat(4, 1fr);
-  }
-  .form-control {
-    width: 50%;
   }
 </style>
